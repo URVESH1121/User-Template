cce7eb06318303394d6beaf139ba9f53451caf34
URVESH RADADIYA <urveshradadiya1121@gmail.com>
2023-04-24 14:34:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import Map from "./Map";
-import Modal from "./Modal";
 
 const App = () => {
   return (
     <>
-      <Map/>
-      {/* <Modal/> */}
+      <Map />
     </>
   );
 };
